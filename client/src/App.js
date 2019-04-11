32588bca48aa8305e4e1893063871eca3a16be02
awalid <awalid@webalo.net>
2019-04-11 23:25:47 +0200

    Added client structure

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h1>My React App</h1>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
