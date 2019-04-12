0b6c996d53dd61327782c578c87c17b70dffe7f0
Mohammed Abdelbarry <m7md337@gmail.com>
2019-04-12 23:39:17 +0200

    Add login and profile validation tests

--- backend/validation/login.js
+++ backend/validation/login.js
@@ -4,6 +4,14 @@ const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 
+  if (isEmpty(data)) {
+    errors.error = "Login failed";
+    return {
+      errors,
+      isValid: false
+    };
+  }
+
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
--- backend/validation/profile.js
+++ backend/validation/profile.js
@@ -10,25 +10,55 @@ module.exports = function validateProfileInput(data) {
       location,
       bio
   */
+
+  if (isEmpty(data)) {
+    errors.error = "Failed to edit profile";
+    return {
+      errors,
+      isValid: false
+    };
+  }
+
   data.social = !isEmpty(data.social) ? data.social : {};
   
   console.log(data);
 
-  const twitter = !isEmpty(data.twitter) ? data.twitter : "";
-  const facebook = !isEmpty(data.facebook) ? data.facebook : "";
-  const website = !isEmpty(data.website) ? data.website : "";
+  data.displayName = !isEmpty(data.displayName) ? data.displayName : "";
+  data.bio = !isEmpty(data.bio) ? data.bio : "";
+  data.avatar = !isEmpty(data.avatar) ? data.avatar : "";
+  const twitter = !isEmpty(data.social.twitter) ? data.social.twitter : "";
+  const facebook = !isEmpty(data.social.facebook) ? data.social.facebook : "";
+  const website = !isEmpty(data.social.website) ? data.social.website : "";
 
-  if (!Validator.isEmpty(twitter) && !Validator.isURL(twitter)) {
+  if (!isEmpty(data.bio) && !Validator.isLength(data.bio, { min: 0, max: 100 })) {
+    errors.bio = "Bio should be less than 100 characters";
+  }
+
+  if (!isEmpty(data.displayName) && !Validator.isLength(data.displayName, { min: 3, max: 100 })) {
+    errors.displayName = "Display name should be more than 3 characters and less than 100 characters";
+  }
+
+  if (!isEmpty(data.avatar) && !Validator.isURL(data.avatar)) {
+    errors.avatar = "Invalid avatar URL";
+  }
+
+  if (!isEmpty(twitter) && !Validator.isURL(twitter)) {
     errors.twitter = "Invalid twitter URL";
   }
 
-  if (!Validator.isEmpty(facebook) && !Validator.isURL(facebook)) {
+  if (!isEmpty(facebook) && !Validator.isURL(facebook)) {
     errors.facebook = "Invalid facebook URL";
   }
 
-  if (!Validator.isEmpty(website) && !Validator.isURL(website)) {
+  if (!isEmpty(website) && !Validator.isURL(website)) {
     errors.website = "Invalid website URL";
   }
+
+  if (!isEmpty(errors)) {
+    errors.error = "Failed to edit profile";
+  }
+
+  console.log(errors)
 
   return {
     errors,
