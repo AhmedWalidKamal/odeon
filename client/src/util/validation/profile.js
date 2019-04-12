699c0ae0aaf2b231b9c9bcb5431630d431d5eb19
Mohammed Abdelbarry <m7md337@gmail.com>
2019-04-13 01:19:44 +0200

    Add validations to front-end

---
+++ client/src/util/validation/profile.js
@@ -0,0 +1,37 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+  /*
+  displayName,
+      avatar,
+      social,
+      location,
+      bio
+  */
+  data.social = !isEmpty(data.social) ? data.social : {};
+
+  console.log(data);
+
+  const twitter = !isEmpty(data.twitter) ? data.twitter : "";
+  const facebook = !isEmpty(data.facebook) ? data.facebook : "";
+  const website = !isEmpty(data.website) ? data.website : "";
+
+  if (!Validator.isEmpty(twitter) && !Validator.isURL(twitter)) {
+    errors.twitter = "Invalid twitter URL";
+  }
+
+  if (!Validator.isEmpty(facebook) && !Validator.isURL(facebook)) {
+    errors.facebook = "Invalid facebook URL";
+  }
+
+  if (!Validator.isEmpty(website) && !Validator.isURL(website)) {
+    errors.website = "Invalid website URL";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
