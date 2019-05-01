780667936ee3ab089b795d653b1faf1168ad5a2e
AhmedWalidKamal <ahmed3walid96@gmail.com>
2019-05-01 19:22:05 +0200

    Modified navbar to link to other urls

--- client/src/App.js
+++ client/src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
         <Navbar />
         <div className="App">
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/profile" component={ProfileCard} />
